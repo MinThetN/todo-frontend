fa7af1f25b55271901fb75e4568f7ce6552f9db5
MinThetN <minthetn15@gmail.com>
2025-01-26 22:34:45 +0700

    fixed

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="bg-blue-500 text-white text-center p-9">
+        <h1>Todo Task Management System</h1>
+    </header>
+  )
+}
